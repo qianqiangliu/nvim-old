ffbf59408866dc7ad295fb823788e1295b83490f
Qianqiang Liu <qianqiangliu@hotmail.com>
2023-12-01 09:27:09 +0800

    set numberwidth=1 and tags=./.tags;,.tags
    Signed-off-by: Qianqiang Liu <qianqiangliu@hotmail.com>

--- lua/options.lua
+++ lua/options.lua
@@ -1,4 +1,5 @@
 vim.opt.number = true
+vim.opt.numberwidth = 1
 vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamed"
 vim.opt.shiftwidth = 4
@@ -11,6 +12,7 @@ vim.opt.cinoptions = ":0,l1,t0,g0,(0"
 vim.opt.grepprg = "rg --vimgrep"
 vim.opt.grepformat = "%f:%l:%c:%m"
 vim.opt.cursorline = true
+vim.opt.tags = "./.tags;,.tags"
 
 -- netrw
 vim.g.netrw_banner = 0
