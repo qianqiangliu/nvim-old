3aba6ed3079a23bc6c49db176ed88e054828ec22
Qianqiang Liu <qianqiangliu@hotmail.com>
2023-12-01 13:46:44 +0800

    map '-' to Oil
    Signed-off-by: Qianqiang Liu <qianqiangliu@hotmail.com>

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -19,6 +19,9 @@ vim.keymap.set('n', '<leader>y', require('osc52').copy_operator, {expr = true})
 vim.keymap.set('n', '<leader>yy', '<leader>c_', {remap = true})
 vim.keymap.set('v', '<leader>y', require('osc52').copy_visual)
 
+-- Oil
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
 -- FTerm
 vim.keymap.set('n', '<leader>t', '<CMD>lua require("FTerm").toggle()<CR>')
 vim.keymap.set('t', '<leader>t', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
