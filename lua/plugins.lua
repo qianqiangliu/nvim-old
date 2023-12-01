3eab33596f78c36138d098fcc5a9e4cba8f2007b
Qianqiang Liu <qianqiangliu@hotmail.com>
2023-12-01 10:11:24 +0800

    add nvim-treesitter plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,6 +9,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     'nvim-lualine/lualine.nvim',
+    'nvim-treesitter/nvim-treesitter',
     {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.5',
